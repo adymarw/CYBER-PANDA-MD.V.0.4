922bdb6a6e77a2e2199f373f4c5197e555157169
Adymarw <164055141+adymarw@users.noreply.github.com>
2024-03-23 14:17:08 +0500

    Update config.js

--- config.js
+++ config.js
@@ -21,10 +21,10 @@ global.prefa = ["."];                                    //Default prefix here. 
 
 
 //
-global.Owner = ["+94704274987", "+94704274987"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.OwnerNumber = ["+94704274987", "+94704274987"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
-global.ownertag = ["+94704274987"];
-global.OwnerName = "SACHITH";
+global.Owner = ["+923137632819"];         //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.OwnerNumber = ["+923137632819"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
+global.ownertag = ["+923137632819"];
+global.OwnerName = "ADY";
 global.BotName = "SACHITH";
 global.packname = "🦋 │𝐂𝐘𝐁𝜩𝐑│𝐏𝜟𝐍𝐃𝐀│𝐌𝐃│𝐕➂ 🦋";                             //Do not change.
 global.author = "SACHITH";                               //Do not change.
